78e547b1abc168e69871118c40274ee05ec41a01
Kostya Voronovich <kostya.voronovich@mail.ru>
2022-04-03 17:39:37 +0300

    added /tokens/create handler

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,18 @@
-from pydantic import BaseModel
+from models import *
+from pydantic import BaseModel, validator
 
 
 class TokenForm(BaseModel):
     media_url: str
     owner: str
+
+
+class TokenInDB(BaseModel):
+    id: int
+    unique_hash: str
+    tx_hash: str
+    media_url: str
+    owner: str
+
+    class Config:
+        orm_mode = True
--- app/handlers.py
+++ app/handlers.py
@@ -1,8 +1,9 @@
+from distutils.log import error
 import os
 from fastapi import APIRouter, Body
 from web3 import Web3
-from forms import TokenForm
-from models import add_token, add_tx_hash_to_token
+from forms import TokenForm, TokenInDB
+from models import add_token, add_tx_hash_to_token, get_token_from_unique_hash
 import secrets
 import string
 
@@ -22,11 +23,48 @@ def get_unique_hash() -> str:
     return crypt_rand_string
 
 
+def build_transaction(web3, owner, media_url, unique_hash):
+    contract = web3.eth.contract(
+        address=os.getenv("CONTRACT_ADDRESS"), abi=os.getenv("ABI")
+    )
+    nonce = web3.eth.get_transaction_count(owner)
+    contract_txn = contract.functions.mint(
+        owner,
+        unique_hash,
+        media_url,
+    ).buildTransaction(
+        {
+            "chainId": web3.eth.chain_id,
+            "gas": 279000,
+            "maxFeePerGas": web3.toWei("2", "gwei"),
+            "maxPriorityFeePerGas": web3.toWei("1", "gwei"),
+            "nonce": nonce,
+        }
+    )
+    return contract_txn
+
+
 @router.post("/create", name="token:create")
 def create(token_form: TokenForm = Body(..., embed=True)):
     unique_hash = get_unique_hash()
     add_token(unique_hash, token_form.media_url, token_form.owner)
-    return {"media_url": token_form.media_url, "owner": token_form.owner}
+    web3 = get_web3()
+    contract_txn = ""
+    try:
+        contract_txn = build_transaction(
+            web3, token_form.owner, token_form.media_url, unique_hash
+        )
+    except ValueError as e:
+        return {"error": e["message"]}
+    signed_txn = web3.eth.account.sign_transaction(
+        contract_txn, private_key=os.getenv("PRIVATE_KEY")
+    )
+    web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+    tx_hash = web3.toHex(web3.keccak(signed_txn.rawTransaction))
+    add_tx_hash_to_token(unique_hash, tx_hash)
+    token = get_token_from_unique_hash(unique_hash)
+    result = TokenInDB.from_orm(token)
+    return result
 
 
 @router.get("/total_supply")
